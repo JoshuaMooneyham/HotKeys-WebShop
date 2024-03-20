18416ae22105b731b5a14958b86ac642e59d3729
JoshuaMooneyham <jmooneyham58@gmail.com>
2024-03-19 20:09:05 -0500

    added landing

---
+++ src/components/landing-welcome/LandingWelcome.jsx
@@ -0,0 +1,10 @@
+function LandingWelcome() {
+    <div className='welcome-container'>
+        {/* Need Image from Kera */}
+        <img src='' />
+            <div className='featured'>
+                <h1 className='featured-title'>FEATURED</h1>
+                <FeaturedCard />
+            </div>
+    </div>
+}
