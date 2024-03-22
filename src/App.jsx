942f83e42f4ef423f73d76859110c5359cffefcb
JoshuaMooneyham <jmooneyham58@gmail.com>
2024-03-21 19:58:22 -0500

    wip delete-entry function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import LandingWelcome from './components/landing-welcome/LandingWelcome';
 import Header from './components/header/Header';
 import CreateEntry from './components/create-entry/CreateEntry';
+import RenderAllItems from './components/delete-entry/DeleteEntry';
+import { GetData } from './components/crud-requests/CrudRequests';
 
 function App() {
+  const data = GetData();
   return(
     <div className='app-container'>
       <Header/>
       <LandingWelcome />
       <CreateEntry />
+      <RenderAllItems ITEMS={data}/>
     </div>
   );
 }
--- src/components/delete-entry/DeleteEntry.jsx
+++ src/components/delete-entry/DeleteEntry.jsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
 
-function ItemCard() {
-    
+function ItemCard({item}) {
+    const [clicked, setClicked] = useState(false);
+
+    return(
+        <div className={clicked ? 'clicked removal-card' : 'removal-card'} onClick={() => {setClicked(!clicked)}} onClick={(e) => {console.log(e)
+        e.target.className = 'clicked'}}>
+            <p>{item.title}</p>
+        </div>
+    );
+}
+
+export default function RenderAllItems({ITEMS}) {
+    return (
+    <div className="removal-container" onClick={e => console.log(e)}>
+        {ITEMS.map((indiv) => <ItemCard item={indiv}/>)}
+    </div>
+    )
 }
