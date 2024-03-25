4da18c5b0727ebca66258e84e76b34c1c70f4bd6
Nathan-T-Moss <nmoss2@basecampcodingacademy.org>
2024-03-24 22:08:05 -0500

    css changes / renaming classNames

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,9 +3,21 @@ import { useState } from "react";
 function CartItem({item, cart, setCart}) {
     const [test, toggleTest] = useState(false);
     console.log(cart)
+
+
+    // placeholder image in case of bad url
+    const [placeholderImg, setPlaceHolderImg] = useState('For Invalid Images')
+    const setRandomPlaceholderImage = (e) => {
+        setPlaceHolderImg('https://picsum.photos/200');
+        // setPlaceHolderImg('https://placehold.co/400');
+        e.currentTarget.src = placeholderImg;
+    }
+
+    
+
     return(
         <div className="cart-container">
-            <form claassName= "cart-card"onSubmit={() => {
+            <form className= "cart-card" onSubmit={() => {
                 console.log(cart, cart.indexOf(item));
                 if (cart.indexOf(item) !== -1) { 
                     cart.splice(cart.indexOf(item), 1);
@@ -13,7 +25,7 @@ function CartItem({item, cart, setCart}) {
                     setCart(cart);
                     toggleTest(!test);
             }}}>
-            <img src={item.images[0]} alt="gfn" />
+            <img src={item.images[0]} onError={setRandomPlaceholderImage} alt="gfn" />
             <h2>{item.title}</h2>
             <p>{item.description}</p>
             <button type="submit">Remove from cart</button>
@@ -25,10 +37,25 @@ function CartItem({item, cart, setCart}) {
 export default function TestCart ({cart, setCart}) {
 console.log(cart);
 
+    // return (
+    //     <div className= "header-container">
+    //         <h1>CART</h1>
+    //         <div className="cart-items">
+    //             {cart.map((item) => <CartItem item={item} cart={cart} setCart={setCart}/>)}
+    //         </div>
+
+    //     </div>
+    // );
+
     return (
-        <div className= "header-container">
-            <h1>CART</h1>
-            {cart.map((item) => <CartItem item={item} cart={cart} setCart={setCart}/>)}
+        <div className="overall-cart-container">
+            <div className="cart-header">
+                <h1>CART</h1>
+            </div>
+            <div className="cart-items">
+                {cart.map((item) => <CartItem item={item} cart={cart} setCart={setCart}/>)}
+            </div>
+
         </div>
     );
 }
